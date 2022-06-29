9b764f65988f6e1afb1b29cec71e269db8f7f573
sergei.mikhan <sergei@origins-digital.com>
2022-06-29 11:01:42 +0300

    fix: correct package file references

--- samples/ios/spm/hbssdk/Package.swift
+++ samples/ios/spm/hbssdk/Package.swift
@@ -16,19 +16,23 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "HBSSDK",
-      path: "xcframeworks/HBSSDK.xcframework"
+      url: "https://origins-mobile-products.s3.eu-west-1.amazonaws.com/hbssdk/whitelabel/1.0.65/HBSSDK.xcframework.zip",
+      checksum: "c72df007ef1a8af7e167b2b1c8bd1fbf90301c25808c5f5cafe2e943270502ef"
     ),
     .binaryTarget(
       name: "OnRewindSDK",
-      path: "xcframeworks/OnRewindSDK.xcframework"
+      url: "https://origins-mobile-products.s3.eu-west-1.amazonaws.com/OnRewindSDK.xcframework.zip",
+      checksum: "7c7f5badcf0e9657eb1bcbccdadd88acfda872b59761cf3510aa9f820bc93c83"
     ),
     .binaryTarget(
       name: "SportBuff",
-      path: "xcframeworks/SportBuff.xcframework"
+      url: "https://buffup-public.s3.eu-west-2.amazonaws.com/ios-sdk/sportbuff-ios-sdk-0.2.0.0.zip",
+      checksum: "eba418bfb5cf938203be14ee5070baa0cdd15df95de70030e6196357e8ff7a54"
     ),
     .binaryTarget(
       name: "hbsshared",
-      path: "xcframeworks/hbsshared.xcframework"
+      url: "https://origins-mobile-products.s3.eu-west-1.amazonaws.com/hbssdk/whitelabel/1.0.65/hbsshared.xcframework.zip",
+      checksum: "bcb0d8177e722b374da3984ad234888ca2e7afe929c3765a7263e294e7b274bd"
     )
   ],
   swiftLanguageVersions: [.v5]
