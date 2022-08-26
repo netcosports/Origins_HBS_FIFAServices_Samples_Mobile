0bad76dd5f9a74a3ac466da0fcc5a6d969373245
sergei.mikhan <sergei@origins-digital.com>
2022-08-26 11:03:14 +0300

    feat(ios): update hbssdk in spm sample

--- samples/ios/spm/hbssdk/Package.swift
+++ samples/ios/spm/hbssdk/Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
                      name: "HBSSDK",
-                     url: "https://origins-mobile-products.s3.eu-west-1.amazonaws.com/hbssdk/whitelabel/1.0.83/HBSSDK.xcframework.zip",
-                     checksum: "69986daa6fd50a361102ddbf34c02952a8eee8160880578b362cc9a307857647"
+                     url: "https://origins-mobile-products.s3.eu-west-1.amazonaws.com/hbssdk/whitelabel/1.0.86/HBSSDK.xcframework.zip",
+                     checksum: "7aba2f29b6adf934a4dd51acb61b7e2be0f8f2c64daae850bd58de12f05beb31"
                     ),
-.binaryTarget(
-                            name: "hbsshared",                                        
-                            url: "https://origins-mobile-products.s3.eu-west-1.amazonaws.com/hbssdk/whitelabel/1.0.83/hbsshared.xcframework.zip",
-                            checksum: "8910cf6b6c1ae32ed2ae7f95b400c0d2f2ae252487ae955d29b3134fd7e355ce"
+        .binaryTarget(
+                            name: "hbsshared",
+                            url: "https://origins-mobile-products.s3.eu-west-1.amazonaws.com/hbssdk/whitelabel/1.0.86/hbsshared.xcframework.zip",
+                            checksum: "f0b2138d29c0a1180b162ff50c5e661cb5f6e3a7aa84d4d827c426e574a3f779"
                           )
     ],
     swiftLanguageVersions: [.v5]
