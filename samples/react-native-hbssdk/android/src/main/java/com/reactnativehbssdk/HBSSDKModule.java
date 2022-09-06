437fe0be96707146d5d75337605ac55d09c9d2f7
Denis Shikunets <denis@origins-digital.com>
2022-09-06 15:10:37 +0300

    fix compilation

--- samples/react-native-hbssdk/android/src/main/java/com/reactnativehbssdk/HBSSDKModule.java
+++ samples/react-native-hbssdk/android/src/main/java/com/reactnativehbssdk/HBSSDKModule.java
@@ -6,9 +6,6 @@ import androidx.annotation.Nullable;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
-
-import com.originsdigital.hbswidgets.standings.StandingsWidget;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,18 +33,19 @@ public class HBSSDKModule extends ReactContextBaseJavaModule {
     public Map<String, Object> getConstants() {
         Map<String, Object> map = new HashMap<String, Object>();
 
+        map.put("standingsComponentHeight", 290);
+        map.put("videosComponentHeight", 248);
+        map.put("mediumMatchesComponentHeight", 95);
+        map.put("smallMatchesComponentHeight", 130);
+
         map.put("teamMatchesComponentHeight", 260);
         map.put("topPlayerStatsComponentHeight", 260);
-        map.put("videosComponentHeight", 260);
-        map.put("standingsComponentHeight", 330);
         map.put("favoritesComponentHeight", 260);
         map.put("championshipComponentHeight", 260);
         map.put("headToHeadComponentHeight", 260);
         map.put("matchCenterComponentHeight", 260);
         map.put("matchesWithEventsComponentHeight", 260);
         map.put("matchesWithoutEventsComponentHeight", 260);
-        map.put("mediumMatchesComponentHeight", 260);
-        map.put("smallMatchesComponentHeight", 260);
         map.put("teamMatchesStatsComponentHeight", 260);
         map.put("teamBoardComponentHeight", 260);
         map.put("venueComponentHeight", 260);
